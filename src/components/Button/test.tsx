ede00b3c5cf5c5cadcb769952679ae9d6971fc3b
RafaPalau <rafaeldonizetip@gmail.com>
2021-07-20 15:39:38 -0300

    component Button complete

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,44 @@
+import { screen } from '@testing-library/react'
+import { renderWitheTheme } from 'utils/tests/helpers'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+
+import Button from '.'
+
+describe('<Button />', () => {
+  it('should render the medium size by default', () => {
+  const {container } = renderWitheTheme(<Button>Buy Now</Button>)
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem',
+    })
+    expect(container.firstChild).toMatchSnapshot()
+   })
+   it('should render the small size', () => {
+    renderWitheTheme(<Button size="small">Buy Now</Button>)
+     expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+       height: '3rem',
+       'font-size': '1.2rem',
+     })
+    })
+    it('should render the large size', () => {
+      renderWitheTheme(<Button size="large">Buy Now</Button>)
+       expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+         height: '5rem',
+         'font-size': '1.6rem',
+         padding: '0.8rem 4.8rem',
+       })
+      })
+  it('should render a fullWidh version', () => {
+    renderWitheTheme(<Button fullWidth >Buy Now</Button>)
+      expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+        width: '100%',
+      })
+    })
+
+  it('should render an icon version', () => {
+    renderWitheTheme(<Button icon={<AddShoppingCart data-testid="icon"/>} >Buy Now</Button>)
+      expect(screen.getByText(/Buy Now/i)).toBeInTheDocument()
+      expect(screen.getByTestId('icon')).toBeInTheDocument()
+    })
+  })
--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -11,6 +11,7 @@ export type HeadingProps = {
 const Heading = ({children, color='white' , lineLeft= false, lineBottom= false}: HeadingProps) => (
   <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
    {children}
+  
   </S.Wrapper>
 )
 
