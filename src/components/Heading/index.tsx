c1a76a1cd63150cc7d52610b6b127dcb18def062
RafaPalau <rafaeldonizetip@gmail.com>
2021-08-05 18:23:41 -0300

    testing heading lineColor

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,15 +1,25 @@
 import { ReactNode } from 'react'
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium' 
 }
 
-const Heading = ({children, color='white' , lineLeft= false, lineBottom= false}: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+const Heading = ({children, color='white' , lineLeft= false, lineBottom= false, lineColor="primary", size="medium"}: HeadingProps) => (
+  <S.Wrapper
+   color={color}
+    lineLeft={lineLeft}
+     lineBottom={lineBottom}
+     size={size}
+     lineColor={lineColor}
+     >
    {children}
   
   </S.Wrapper>
--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -17,12 +17,45 @@ describe('<Heading />', () => {
 
     it('should render a heading with a line to the left side', () => {
       renderWitheTheme(<Heading lineLeft>Juju Soap</Heading>)
-      expect(screen.getByRole('heading', {name: /Juju Soap/i})).toHaveStyle({'borderLeft': "0.7rem solid #3CD3C1"})
+      expect(screen.getByRole('heading', {name: /Juju Soap/i})).toHaveStyle({'borderLeft': "0.7rem solid #F231A5"})
       })
       
-      it('should render a heading with a line at the bottom', () => {
-        renderWitheTheme(<Heading lineBottom>Juju Soap</Heading>)
-        expect(screen.getByRole('heading', {name: /Juju Soap/i})).toHaveStyleRule( 'border-bottom', '0.5rem solid #F231A5' , {modifier: '::after'})
+    it('should render a heading with a line at the bottom', () => {
+      renderWitheTheme(<Heading lineBottom>Juju Soap</Heading>)
+      expect(screen.getByRole('heading', {name: /Juju Soap/i})).toHaveStyleRule( 'border-bottom', '0.5rem solid #F231A5' , {modifier: '::after'})
+    })
+
+    it('should render a heading with a small size', () => {
+      renderWitheTheme(<Heading size="small">Juju Soap</Heading>)
+      expect(screen.getByRole('heading', {name: /Juju Soap/i})).toHaveStyle( {'font-size': '1.6rem'})
+      expect(screen.getByRole('heading', {name: /Juju Soap/i})).toHaveStyleRule( 'width', '3rem' , {modifier: '::after'})
+    }) 
+    it('should render a Heading with a primary line color', () => {
+      renderWitheTheme(
+        <Heading lineColor="primary" lineLeft lineBottom>
+          Lorem Ipsum
+        </Heading>
+      )
+  
+      const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+      expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+      expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+        modifier: '::after'
       })
+    })
+  
+    it('should render a Heading with a secondary line color', () => {
+      renderWitheTheme(
+        <Heading lineColor="secondary" lineLeft lineBottom>
+          Lorem Ipsum
+        </Heading>
+      )
+  
+      const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+      expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+      expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+        modifier: '::after'
+      })
+    })
   })
 
