8133fd12477c5cd154afe38fbcb2ad2997a34292
RafaPalau <rafaeldonizetip@gmail.com>
2021-08-17 11:58:50 -0300

    button component with tests, storybook ...

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,32 +3,29 @@ import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
 
 type WrapperProps = {
-  hasIcon: boolean,
+  hasIcon: boolean
 } & Pick<ButtonProps, 'size' | 'fullWidth'>
 
 const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
-  height: 3rem;
-  font-size: ${theme.font.sizes.xsmall};
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
   `,
   medium: (theme: DefaultTheme) => css`
-   height: 4rem;
-  font-size: ${theme.font.sizes.small};
-  padding: ${theme.spacings.xxsmall} ${theme.spacings.medium}; ;
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium}; ;
   `,
- large: (theme: DefaultTheme) => css`
-  height: 5rem;
-  font-size: ${theme.font.sizes.medium};
-  padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
-   `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `,
   fullWidth: () => css`
     width: 100%;
   `,
   withIcon: (theme: DefaultTheme) => css`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    svg{
+    svg {
       width: 1.5rem;
 
       & + span {
@@ -38,16 +35,24 @@ const wrapperModifiers = {
   `
 }
 export const Wrapper = styled.button<WrapperProps>`
-${({theme, size, fullWidth, hasIcon}) => css`
-background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
-color: ${theme.colors.white};
-border: 0;
-cursor: pointer;
-border-radius: ${theme.border.radius};
-padding: ${theme.spacings.xxsmall};
+  ${({ theme, size, fullWidth, hasIcon }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+    color: ${theme.colors.white};
+    border: 0;
+    cursor: pointer;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+    text-decoration: none;
 
-${!!size && wrapperModifiers[size](theme)}
-${!!fullWidth && wrapperModifiers.fullWidth()}
+    &:hover {
+      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+    }
+
+    ${!!size && wrapperModifiers[size](theme)}
+    ${!!fullWidth && wrapperModifiers.fullWidth()}
 ${!!hasIcon && wrapperModifiers.withIcon(theme)}
-`}
+  `}
 `
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -6,39 +6,53 @@ import Button from '.'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
-  const {container } = renderWitheTheme(<Button>Buy Now</Button>)
+    const { container } = renderWitheTheme(<Button>Buy Now</Button>)
     expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
-      'font-size': '1.4rem',
+      'font-size': '1.4rem'
     })
     expect(container.firstChild).toMatchSnapshot()
-   })
-   it('should render the small size', () => {
+  })
+  it('should render the small size', () => {
     renderWitheTheme(<Button size="small">Buy Now</Button>)
-     expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
-       height: '3rem',
-       'font-size': '1.2rem',
-     })
-    })
-    it('should render the large size', () => {
-      renderWitheTheme(<Button size="large">Buy Now</Button>)
-       expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
-         height: '5rem',
-         'font-size': '1.6rem',
-         padding: '0.8rem 4.8rem',
-       })
-      })
-  it('should render a fullWidh version', () => {
-    renderWitheTheme(<Button fullWidth >Buy Now</Button>)
-      expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
-        width: '100%',
-      })
-    })
-
-  it('should render an icon version', () => {
-    renderWitheTheme(<Button icon={<AddShoppingCart data-testid="icon"/>} >Buy Now</Button>)
-      expect(screen.getByText(/Buy Now/i)).toBeInTheDocument()
-      expect(screen.getByTestId('icon')).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
     })
   })
+  it('should render the large size', () => {
+    renderWitheTheme(<Button size="large">Buy Now</Button>)
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '5rem',
+      'font-size': '1.6rem',
+      padding: '0.8rem 4.8rem'
+    })
+  })
+  it('should render a fullWidh version', () => {
+    renderWitheTheme(<Button fullWidth>Buy Now</Button>)
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      width: '100%'
+    })
+  })
+
+  it('should render an icon version', () => {
+    renderWitheTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy Now</Button>
+    )
+    expect(screen.getByText(/Buy Now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should render Button as a link', () => {
+    renderWitheTheme(
+      <Button as="a" href="/link">
+        Buy Now
+      </Button>
+    )
+    expect(screen.getByRole('link', { name: /Buy Now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
+  })
+})
