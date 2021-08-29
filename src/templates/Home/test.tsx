c6f44ce3d3c4358dc23918c7a0bd6f63add1c2a2
RafaPalau <rafaeldonizetip@gmail.com>
2021-08-29 09:02:17 -0300

    test home completed

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,22 +1,42 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
-import Heading from 'components/Heading'
 import { renderWitheTheme } from 'utils/tests/helpers'
+
+import bannerMock from 'components/BannerSlider/mock'
+import itemsMock from 'components/ItemCardSlider/mock'
+import highlightMock from 'components/Highligth/mock'
 
 import Home from '.'
 
+const props = {
+  banners: bannerMock,
+  newItems: [itemsMock[0]],
+  bestSellersHightLight: highlightMock,
+  bestSellerItem: [itemsMock[0]]
+}
+
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWitheTheme(<Home />)
+    renderWitheTheme(<Home {...props} />)
+
+    // Menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    // Footer
     expect(
       screen.getByRole('heading', { name: /contato/i })
     ).toBeInTheDocument()
-  })
 
-  it('should render the sections', () => {
-    renderWitheTheme(<Heading>Novidades</Heading>)
-    expect(screen.getByRole('heading', { name: /Novidades/ })).toHaveStyle({
-      color: '#FAFAFA'
-    })
+    expect(screen.getAllByRole('img', { name: /Juju Soap/i })).toHaveLength(2)
+
+    expect(
+      screen.getByRole('heading', { name: /Novidades/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Mais Vendidos/i })
+    ).toBeInTheDocument()
+    // Banner
+    expect(screen.getAllByText(/Sabonete 1/i)).toHaveLength(1)
   })
 })
